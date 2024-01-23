18a6d19f6764841f0bc1e38fb606a5fb2137a75c
aimei60 <104590763+aimei60@users.noreply.github.com>
2024-01-23 07:24:29 +0000

    Added how the computer picks its word and presents it to the user. Also added user input and defined more variables that will be used later in the game. I also refined the main section of the game.

--- game.py
+++ game.py
@@ -10,15 +10,48 @@
 #if user guesses before 5 attempts, user wins, guessed letter is solo
 #if user doesn't guess and the attempts run out, user loses, print correct word: solo
 
-
 import random
 from words_file import words
 
-def get_valid_word(word):
+def get_valid_word(words):
     word = random.choice(words)
     return word.upper()
 
 def game():
     lives = 6
-    word = get_valid_word(word)
+    word = get_valid_word(words)
+    used_letters = set()
+    incorrect_letters = ""
+    misplaced_letters = ""
+
+    word_list = [letter if letter in used_letters else '-' for letter in word]
+    print("Word: ", " ".join(word_list))
+
+    user_word = input("Enter a word: ").upper()
+    #need to break the word into letters to assess it/ compare later down the line function 
+    print(user_word)
     
+# need to compare the user letter to the computer letter in their words respectively
+    # for any of the positions in the in the word 
+    #if the user letter and the computer letter matches:
+        #reveal the letter and the rest of the unguessed word e.g. S - - -
+    #if the user letter is in the computer word but in the wrong place:
+        #append user letter to misplaced_letters
+        #print misplaced_letters and the computer word so far
+        #lives -= 1
+    # if the letter is not in the computer word:
+        #append the letter to incorrect_letters
+        #print incorrect_letters and the computer word so far
+        #lives -= 1
+    
+#need to add the lives 
+#need to add how the game concludes, user guessed within 5 lives and wins or loses and the word is revealed
+    
+    
+    for letter in word:
+        
+        
+    
+
+game()
+
